148d7c8c82ee9e8e0c0379a503062b94f2e1ce7c
Christie Ballard <christie.ballard@riotinto.com>
2024-08-06 20:38:07 +1000

    Class day 1 of Python exercises

---
+++ Session1/variables.py
@@ -0,0 +1,58 @@
+# print("Hello She Codes!")
+
+# Variables
+# String - text data
+first_name = "asli" #quotes make it text data
+price = 5.60 
+
+# numerical data - integer, floats
+price = 5.60 #floats, fractions
+# age = 29 #interger aka whole number
+year = 2024
+birthyear = 1995
+birthyear = 2000 #if you have the same variable it reads the last entry
+# age = year - birthyear
+#print(age)
+greetings = f"Welcome {first_name}" #f means formatting
+
+# Booleans - true, false (aka 2 values)
+rainy = True # capital T makes it a variable
+fridge_is_empty = False
+
+# None
+None
+
+# name = input("what is your name?")
+# greetings = f"Welcome {name} "
+#print(greetings)
+type_of_greeting = type(greetings)
+#print(type (greetings))
+
+# age = int(input("how old is your dog?"))
+# conversation = f"oh your dog is {age*7} in human age"
+# print(conversation)
+
+sisters_age = 3
+brothers_age = 9
+total_sibling_age = sisters_age + brothers_age
+print(total_sibling_age)
+
+Niki_wallet = -3
+tim_wallet = 9
+wine_money = Niki_wallet + tim_wallet
+print(wine_money)
+
+phoebe_basil = 3
+hannah_basil = -9
+pesto = phoebe_basil + hannah_basil
+print(pesto)
+
+haylee_toys = int(input("how many ras did Haylee get?"))
+chaz_toys = int(input("how many ras did Chaz get?"))
+total_toys = haylee_toys+chaz_toys
+print(total_toys)
+
+Name = input("What is your name?")
+age = int(input("how old are you?"))
+message = f"Hi {Name}, congrats on turning {age}"
+print(message)
